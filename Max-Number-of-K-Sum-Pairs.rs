10d8fad273c72bca725911b168dca4c8084a130e
Jonas Lilletvedt <143400254+JL2023-coder@users.noreply.github.com>
2025-04-20 17:55:52 +0200

    [1679] [Time Beats: 6.77%] [Memory Beats: 19.55%] - LeetPush

---
+++ Max-Number-of-K-Sum-Pairs.rs
@@ -0,0 +1,25 @@
+use std::collections::HashMap;
+use std::cmp::min;
+impl Solution {
+    pub fn max_operations(nums: Vec<i32>, k: i32) -> i32 {
+        let mut max: i32 = 0;
+        let mut map: HashMap<i32, i32> = HashMap::<i32,i32>::new();
+
+        for v in nums {
+            let mut count = map.entry(v).or_insert(0);
+            *count += 1;
+        }
+
+        for (&key, &value) in &map {
+            let to_find = k - key;
+            let value_2 = map.get(&to_find).copied().unwrap_or(0);
+            println!("{}  {}", value, value_2);
+
+            max += min(value, value_2);
+        }
+
+        println!("{:?}", map);
+
+        return max / 2;
+    }
+}
