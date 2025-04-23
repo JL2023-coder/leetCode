b937a4f10ee2e06b611300cdac7cba98662b6c3f
Jonas Lilletvedt <143400254+JL2023-coder@users.noreply.github.com>
2025-04-23 18:00:39 +0200

    [1399] [Time Beats: 100.00%] [Memory Beats: 42.11%] - LeetPush

--- Count-Largest-Group.rs
+++ Count-Largest-Group.rs
@@ -1,34 +1,33 @@
-use std::collections::HashMap;
+
 impl Solution {
     pub fn count_largest_group(n: i32) -> i32 {
-        let mut group_count: HashMap<i32, i32> = HashMap::<i32, i32>::new();
+        let mut group_count: Vec<i32> = vec![0; 37];
         let mut max: i32 = 0;
         let mut count: i32 = 0;
         for i in 1..n + 1 {
-            let digit_sum = digit_sum(i);
-            let c = group_count.entry(digit_sum).or_insert(0);
+            let digit_sum: usize = digit_sum(i);
+            let c = group_count.get_mut(digit_sum).unwrap();
             *c += 1;
-        }
 
-        for c in group_count.values() {
             if *c > max {
                 max = *c;
                 count = 1;
             }
             else if *c == max {
                 count += 1;
-            }
+            }    
         }
+
         return count;
 
-        fn digit_sum(n: i32) -> i32 {
+        fn digit_sum(n: i32) -> usize {
             let mut sum: i32 = 0;
             let mut num = n;
             while num >= 1 {
                 sum += num % 10;
                 num = num / 10;
             }
-            return sum;
+            return sum as usize;
         }
     }
 }
