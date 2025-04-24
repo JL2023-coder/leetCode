9b19aedd17b38d8c2ec9525f105f168a0880e7aa
Jonas Lilletvedt <143400254+JL2023-coder@users.noreply.github.com>
2025-04-25 00:13:35 +0200

    [2799] [Time Beats: 55.56%] [Memory Beats: 22.22%] - LeetPush

--- Count-Complete-Subarrays-in-an-Array.rs
+++ Count-Complete-Subarrays-in-an-Array.rs
@@ -2,33 +2,47 @@ use std::collections::HashMap;
 impl Solution {
     pub fn count_complete_subarrays(nums: Vec<i32>) -> i32 {
         let mut res = 0;
-        let mut map_values = HashMap::<i32, usize>::new();
-        let mut distinct_values = 0;
-        for i in &nums {
-            let c = map_values.entry(*i).or_insert(0);
-            if *c == 0 {
-                distinct_values += 1;
-            }
-            *c += 1;
-        }
 
-        for i in 0..nums.len() { 
-            let mut counter = 0;
-            let mut encountered_values = HashMap::<i32, usize>::new();
-            for j in i..nums.len() {
-                let c = encountered_values.entry(nums[j]).or_insert(0);
-                if *c == 0 {
-                    counter += 1;
+        let mut l = 0;
+        let mut r = 0;
+
+        let mut count = 0;
+        let distinct_values = Self::get_distinct_values(&nums);
+
+        let mut map = HashMap::new();
+
+        for l in 0..nums.len() {
+            while count < distinct_values && r < nums.len() {
+                let r_count = map.entry(nums[r]).or_insert(0);
+                if *r_count == 0 {
+                    count += 1;
                 }
-                *c += 1;
+                *r_count += 1;
+                r += 1;
+            }
 
-                if counter == distinct_values {
-                    res += nums.len() - j;
-                    break;
+            if count == distinct_values {
+                let l_count = map.entry(nums[l]).or_insert(0);
+                *l_count -= 1;
+                if *l_count == 0 {
+                    count -= 1;
                 }
+                res += nums.len() - r + 1;
             }
         }
-
         return res as i32;
+    }
+
+    pub fn get_distinct_values(list: &Vec<i32>) -> i32 {
+        let mut count = 0;
+        let mut map = HashMap::new();
+        for i in list {
+            let my_bool = map.entry(i).or_insert(false);
+            if !*my_bool {
+                count += 1;
+            }
+            *my_bool = true;
+        }
+        return count;
     }
 }
