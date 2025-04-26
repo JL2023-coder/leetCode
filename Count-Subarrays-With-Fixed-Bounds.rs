4e9a5b100e270240940f7deb8f24bcfb62fd779d
Jonas Lilletvedt <143400254+JL2023-coder@users.noreply.github.com>
2025-04-26 21:39:56 +0200

    [2444] [Time Beats: 10.00%] [Memory Beats: -%] - LeetPush

--- Count-Subarrays-With-Fixed-Bounds.rs
+++ Count-Subarrays-With-Fixed-Bounds.rs
@@ -1,27 +1,36 @@
 use std::cmp::min;
 impl Solution {
     pub fn count_subarrays(nums: Vec<i32>, min_k: i32, max_k: i32) -> i64 {
-        let mut mini: i32 = -1;
-        let mut maxi: i32 = -1;
-        let mut start: i32 = -1;
-
+        let mut good_arrs = Vec::new();
         let mut res = 0;
+        let n = nums.len();
 
-        for i in 0..nums.len() {
+        let mut arr = Vec::new();
+        for i in 0..n {
             if nums[i] >= min_k && nums[i] <= max_k {
-                if nums[i] == min_k {
-                    mini = i as i32;
-                }
-                if nums[i] == max_k {
-                    maxi = i as i32;
-                }
-                if mini != -1 && maxi != -1 && min(mini, maxi) > start{
-                    res += (min(mini, maxi) - start) as i64;
-                }
+                arr.push(nums[i]);
             }
             else {
-                start = i as i32;
-            }   
+                if arr.len() != 0 {
+                    good_arrs.push(arr);
+                }
+                arr = Vec::new();
+            }
+        }
+        if arr.len() != 0 {
+            good_arrs.push(arr);
+        }
+
+        for i in good_arrs {
+            let mut mini: i32 = -1;
+            let mut maxi: i32 = -1;
+            let mut start: i32 = -1;
+            for r in 0..i.len() {
+                if i[r] == min_k {mini = r as i32}
+                if i[r] == max_k {maxi = r as i32} 
+                res += (min(mini, maxi) - start) as i64;
+            }
+
         }
 
         return res;
